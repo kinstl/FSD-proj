f921c0fcac47c1b3ee82e94b84a75bfb595e9c48
Konstantin Krivilev <inferno750@mail.ru>
2024-09-30 21:45:52 +0300

    start StarRating

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,10 +1,16 @@
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page/Page';
+import { StarRating } from '@/shared/ui/StarRating/StarRating';
 
 const MainPage = () => {
     const { t } = useTranslation();
 
-    return <Page>{t('Главная страница')}</Page>;
+    return (
+        <Page>
+            {t('Главная страница')}
+            <StarRating size={50} />
+        </Page>
+    );
 };
 
 export default MainPage;
