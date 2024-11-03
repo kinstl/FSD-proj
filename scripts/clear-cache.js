a4696f396a1658ac7772a1cd180db761ec7f520a
Konstantin Krivilev <inferno750@mail.ru>
2024-11-03 15:31:30 +0300

    upgrade webpack config, add babel cache, add clear cache postinstall script

--- scripts/clear-cache.js
+++ scripts/clear-cache.js
@@ -1,2 +1,15 @@
-// path/fs
-console.log('CLEAR CACHE');
+const fs = require('fs');
+const path = require('path');
+
+const cacheDir = path.join(__dirname, '..', 'node_modules', '.cache'); // Adjust the folder path if your cache folder is located elsewhere
+
+function clearCache() {
+    if (fs.existsSync(cacheDir)) {
+        fs.rmSync(cacheDir, { recursive: true, force: true });
+        console.log('Cache folder cleared successfully.');
+    } else {
+        console.log('No cache folder found, skipping.');
+    }
+}
+
+clearCache();
