a5e5afbcfa89fdd997b86659e6b92717458eb0f2
Konstantin Krivilev <inferno750@mail.ru>
2024-11-14 23:54:30 +0300

    fix bash deploy script

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -1,5 +1,5 @@
-cd Ulbi-Frontend
+cd ~/../Ulbi-Frontend
 npm run build:prod
 
 rm -rf ~/../var/www/Ulbi-Frontend/html
-mv Ulbi-Frontend/build ~/../var/www/Ulbi-Frontend/html
+mv ~/../Ulbi-Frontend/build ~/../var/www/Ulbi-Frontend/html
