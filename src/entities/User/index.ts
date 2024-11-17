e33527bb71f427b227e32e627aaf09f0143ba780
Konstantin Krivilev <inferno750@mail.ru>
2024-11-17 15:14:02 +0300

    add json settings for user

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,3 +1,5 @@
+export { saveJsonSettings } from './model/services/saveJsonSettings';
+export { useJsonSettings } from './model/selectors/jsonSettings';
 export { userReducer, userActions } from './model/slice/userSlice';
 export { UserRole } from './model/consts/userConsts';
 export type { UserSchema, User } from './model/types/user';
